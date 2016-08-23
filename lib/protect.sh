8cf3d3b1d09e8f072b3978258101252970d85ef7
soffery <soffery@il.ibm.com>
2016-08-23 14:49:07 +0300

    adding extarnal packages

--- lib/protect.sh
+++ lib/protect.sh
@@ -23,6 +23,10 @@ extract_packages() {
   echo -n { \"UNIX_PKGS\": { \"packages\" :[ >  $DEFENDER_HOME/distro.json
   # packageVersion,packageVersion ...packageVersion
   dpkg-query -W -f='{"pkg":"${Package}_${Version}"},' | sed 's/,$//' >>  $DEFENDER_HOME/distro.json
+  # add aditional packages, written in a file and supply by the buildpack it self 
+  if [  -s $DEFENDER_HOME/importedPackages.csv -a -r $DEFENDER_HOME/importedPackages.csv ]; then
+     awk -F , '{ if(FNR != 1 && $1 != "" && $2 != "" ) { if(FNR != 2){ printf ","} ;printf( "{\"pkg\":\"%s_%s\"},",$1,$2 )} };' $DEFENDER_HOME/importedPackages.csv  | sed 's/,$//' >>  $DEFENDER_HOME/distro.json
+  fi
   # ", ---- end the packageVersion list  
   echo -n ], >>  $DEFENDER_HOME/distro.json
   # "Codename": "trusty", 
@@ -81,6 +85,11 @@ extract_node_modules() {
 	#npm ls -json 2>/dev/null | node $DEFENDER_HOME/NodeProtect.js  
 	cat $DEFENDER_HOME/distro.json | node $DEFENDER_HOME/NodeProtect.js  
 }    
+
+read_imported packages(){
+  export ret=""
+
+}
 
 i=0
 while [ true ]; do
