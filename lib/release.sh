1b880b57bed5ccb007b30ce961983df2a7323373
soffery <soffery@il.ibm.com>
2016-09-08 13:20:54 +0300

    fixing ...

--- lib/release.sh
+++ lib/release.sh
@@ -3,6 +3,6 @@ echo  "see environment variables below :"
 env 
 echo "This is the application HOME directory: ${PWD}"
 echo "The BlueSecure dashboard can be found in the follow URL:" 
-cat ${DEFENDER_HOME}/dash ${DEFENDER_HOME}/sid 
+echo `cat ${DEFENDER_HOME}/dash ${DEFENDER_HOME}/sid `
 
 npm start
