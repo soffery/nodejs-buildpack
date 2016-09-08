553ec07023e39814547b2fa9fb3f3dce42be5f4e
soffery <soffery@il.ibm.com>
2016-09-08 18:22:02 +0300

    enforcer fixup

--- lib/release.sh
+++ lib/release.sh
@@ -1,12 +1,14 @@
 echo " starting the release.sh script "
-echo  "see environment variables below :" 
-env 
+#echo  "see environment variables below :" 
+#env 
 echo "This is the application HOME directory: ${PWD}"
-echo "The BlueSecure dashboard can be found in the follow URL:" 
+echo "The BlueSecure dashboard can be found in the follow URL:"   
 echo `cat ${DEFENDER_HOME}/dash ${DEFENDER_HOME}/sid `
 
 ${DEFENDER_HOME}/protect.sh ${DEFENDER_HOME} "runonce"
-source ${DEFENDER_HOME}/enforcer.sh
-enforce `cat action.txt`
+if [ -f ${DEFENDER_HOME}/action.txt ] ; then 
+	source ${DEFENDER_HOME}/enforcer.sh
+	enforce `cat ${DEFENDER_HOME}/action.txt`
+fi 	
 
 npm start
