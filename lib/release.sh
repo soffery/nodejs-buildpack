9d0b36f4d486c5ead4ee6a92b98b4b91cd84dd38
soffery <soffery@il.ibm.com>
2016-09-08 12:31:38 +0300

    adding enforcer and reset

--- lib/release.sh
+++ lib/release.sh
@@ -1,5 +1,8 @@
 echo " starting the release.sh script "
+echo  "see environment variables below :" 
 env 
-echo "This is the directory ${PWD}" 
+echo "This is the application HOME directory: ${PWD}"
+echo "The BlueSecure dashboard can be found in the follow URL:" 
+cat ${DEFENDER_HOME}/dash ${DEFENDER_HOME}/sid 
 
 npm start
